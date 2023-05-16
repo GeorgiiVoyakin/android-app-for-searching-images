3495260c13067491885c1108d4a4cd32d687911e
Георгий Воякин <voyaking@gmail.com>
2023-05-16 15:58:28 +0300

    Update registration and gallery screens template

--- app/src/main/java/com/github/GeorgiiVoyakin/view/GalleryFragment.kt
+++ app/src/main/java/com/github/GeorgiiVoyakin/view/GalleryFragment.kt
@@ -1,6 +1,23 @@
 package com.github.GeorgiiVoyakin.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.github.GeorgiiVoyakin.R
 
 class GalleryFragment : Fragment() {
+    private lateinit var textView: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_gallery, container, false)
+        textView = view.findViewById(R.id.textView)
+        return view
+    }
 }
--- app/src/main/java/com/github/GeorgiiVoyakin/view/RegistrationFragment.kt
+++ app/src/main/java/com/github/GeorgiiVoyakin/view/RegistrationFragment.kt
@@ -1,6 +1,23 @@
 package com.github.GeorgiiVoyakin.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.github.GeorgiiVoyakin.R
 
 class RegistrationFragment : Fragment() {
+    private lateinit var textView: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_registration, container, false)
+        textView = view.findViewById(R.id.textView2)
+        return view
+    }
 }
