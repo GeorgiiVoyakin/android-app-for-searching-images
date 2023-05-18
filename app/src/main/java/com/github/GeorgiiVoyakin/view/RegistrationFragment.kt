158bd701a0e9952bb1de49944b5fc80631d6c01d
Георгий Воякин <voyaking@gmail.com>
2023-05-18 23:38:43 +0300

    Update navigation

--- app/src/main/java/com/github/GeorgiiVoyakin/view/RegistrationFragment.kt
+++ app/src/main/java/com/github/GeorgiiVoyakin/view/RegistrationFragment.kt
@@ -5,14 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.github.GeorgiiVoyakin.R
+import androidx.navigation.findNavController
+import com.github.GeorgiiVoyakin.databinding.FragmentRegistrationBinding
 
 class RegistrationFragment : Fragment() {
+    private lateinit var binding: FragmentRegistrationBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+    ): View {
+        binding = FragmentRegistrationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.loginButton.setOnClickListener {
+            // Переходим на главный экран
+            val action =
+                RegistrationFragmentDirections
+                    .actionRegistrationFragmentToMainFragment()
+            it.findNavController().navigate(action)
+        }
     }
 }
