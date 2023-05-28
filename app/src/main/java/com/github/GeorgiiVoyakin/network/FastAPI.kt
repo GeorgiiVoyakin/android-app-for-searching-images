62544bebb46745f75b132f6944f99e8a4192fad4
Георгий Воякин <voyaking@gmail.com>
2023-05-29 00:43:04 +0300

    Add request on login button click, update manifest to perform network requests

---
+++ app/src/main/java/com/github/GeorgiiVoyakin/network/FastAPI.kt
@@ -0,0 +1,118 @@
+package com.github.GeorgiiVoyakin.network
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface FastAPI {
+    @FormUrlEncoded
+    @POST("/token")
+    fun loginForAccessToken(
+        @Field("username")
+        username: String,
+        @Field("password")
+        password: String
+    ): Call<Token>
+
+//    @GET("/users/me")
+//    fun readUsersMe(): Call<User>
+//
+//    @GET("/users/me/images/")
+//    fun readOwnImages(): Call<List<Image>>
+
+//    @GET("/users/")
+//    fun readUsers(
+//        @Query("skip") skip: Int = 0,
+//        @Query("limit") limit: Int = 100
+//    ): Call<Response_Read_Users_Users_Get>
+
+//    @POST("/users/")
+//    fun createUser(@Body userCreate: UserCreate): Call<User>
+//
+//    @GET("/users/{user_id}")
+//    fun readUser(@Path("user_id") userId: Int): Call<User>
+
+//    @POST("/users/{user_id}/images/")
+//    fun createImageForUser(
+//        @Path("user_id") userId: Int,
+//        @Body requestBody: Body_create_image_for_user_users__user_id__images__post
+//    ): Call<Image>
+
+//    @GET("/images/")
+//    fun readImages(
+//        @Query("skip") skip: Int = 0,
+//        @Query("limit") limit: Int = 100
+//    ): Call<Response_Read_Images_Images_Get>
+
+//    @POST("/images/{image_id}/objects")
+//    fun addObjectToImage(
+//        @Path("image_id") imageId: Int,
+//        @Query("object") objectName: String
+//    ): Call<ImageObject>
+
+//    @GET("/images/{object}")
+//    fun readImagesByObject(
+//        @Path("object") objectName: String,
+//        @Query("skip") skip: Int = 0,
+//        @Query("limit") limit: Int = 100
+//    ): Call<Response_Read_Images_By_Object_Images_Object_Get>
+
+//    @GET("/users/me/images/{object}")
+//    fun readOwnImagesByObject(
+//        @Path("object") objectName: String,
+//        @Query("user_id") userId: Int,
+//        @Query("skip") skip: Int = 0,
+//        @Query("limit") limit: Int = 100
+//    ): Call<Response_Read_Own_Images_By_Object_Users_Me_Images_Object_Get>
+
+//    @GET("/images/albums/")
+//    fun readAlbums(
+//        @Query("skip") skip: Int = 0,
+//        @Query("limit") limit: Int = 100
+//    ): Call<Response_Read_Albums_Images_Albums_Get>
+//
+//    @GET("/users/me/images/albums/")
+//    fun readOwnAlbums(
+//        @Query("user_id") userId: Int,
+//        @Query("skip") skip: Int = 0,
+//        @Query("limit") limit: Int = 100
+//    ): Call<Response_Read_Own_Albums_Users_Me_Images_Albums_Get>
+//
+//    @POST("/users/{user_id}/images/albums/")
+//    fun createAlbumForUser(
+//        @Path("user_id") userId: Int,
+//        @Body albumCreate: AlbumCreate
+//    ): Call<Album>
+
+//    @POST("/users/{user_id}/images/albums/{album_id}")
+//    fun addImageToAlbum(
+//        @Path("user_id") userId: Int,
+//        @Path("album_id") albumId: Int,
+//        @Query("image_id") imageId: Int
+//    ): Call<Album>
+//
+//    @GET("/users/me/images/favorites/")
+//    fun readOwnFavoriteImages(
+//        @Query("user_id") userId: Int,
+//        @Query("skip") skip: Int = 0,
+//        @Query("limit") limit: Int = 100
+//    ): Call<Response_Read_Own_Favorite_Images_Users_Me_Images_Favorites_Get>
+//
+//    @POST("/users/{user_id}/images/{image_id}/favorites")
+//    fun addImageToFavorites(
+//        @Path("user_id") userId: Int,
+//        @Path("image_id") imageId: Int
+//    ): Call<ApiResponse>
+//
+//    @DELETE("/users/{user_id}/images/{image_id}/favorites")
+//    fun removeImageFromFavorites(
+//        @Path("user_id") userId: Int,
+//        @Path("image_id") imageId: Int
+//    ): Call<ApiResponse>
+}
--- app/src/main/java/com/github/GeorgiiVoyakin/view/LoginFragment.kt
+++ app/src/main/java/com/github/GeorgiiVoyakin/view/LoginFragment.kt
@@ -2,13 +2,25 @@ package com.github.GeorgiiVoyakin.view
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.github.GeorgiiVoyakin.databinding.FragmentLoginBinding
+import com.github.GeorgiiVoyakin.network.FastAPI
+import com.github.GeorgiiVoyakin.network.Token
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val TAG = "LoginFragment"
+private const val BASE_URL = "http://10.0.2.2:8000"
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
@@ -30,11 +42,7 @@ class LoginFragment : Fragment() {
         }
 
         binding.loginButton.setOnClickListener {
-            // Переходим на главный экран
-            val action =
-                LoginFragmentDirections
-                    .actionLoginFragmentToGalleryFragment()
-            it.findNavController().navigate(action)
+            performLogin()
         }
 
         binding.registerButton.setOnClickListener {
@@ -59,4 +67,47 @@ class LoginFragment : Fragment() {
         return sharedPreferences?.getBoolean("is_logged_in", false) ?: false
     }
 
+    private fun performLogin() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val api: FastAPI = retrofit.create(FastAPI::class.java)
+        val call = api.loginForAccessToken(
+            binding.login.text.toString(),
+            binding.password.text.toString()
+        )
+
+        call.enqueue(object : Callback<Token> {
+            override fun onResponse(call: Call<Token>, response: Response<Token>) {
+                Log.d(TAG, "onResponse: $response")
+                if (response.isSuccessful) {
+                    Log.d(TAG, "onResponse: response is successful ${response.body()}")
+                    val action =
+                        LoginFragmentDirections
+                            .actionLoginFragmentToGalleryFragment()
+                    findNavController().navigate(action)
+                } else {
+                    Log.d(TAG, "onResponse: response is not successful")
+                    Toast.makeText(
+                        activity,
+                        "Не верное имя пользователя или пароль",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            }
+
+            override fun onFailure(call: Call<Token>, t: Throwable) {
+                Toast.makeText(
+                    activity,
+                    "Не удалось выполнить вход, проверьте интернет соединение",
+                    Toast.LENGTH_LONG
+                )
+                    .show()
+                Log.d(TAG, "onFailure: not ok, ${t.message}")
+            }
+        })
+    }
 }
