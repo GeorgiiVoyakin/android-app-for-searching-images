db6c3ed44123a87aef698abd8c5857842af01fc2
Георгий Воякин <voyaking@gmail.com>
2023-05-29 01:49:34 +0300

    Update layout, add logout button

--- app/src/main/java/com/github/GeorgiiVoyakin/view/SettingsFragment.kt
+++ app/src/main/java/com/github/GeorgiiVoyakin/view/SettingsFragment.kt
@@ -1,14 +1,19 @@
 package com.github.GeorgiiVoyakin.view
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.github.GeorgiiVoyakin.R
 import com.github.GeorgiiVoyakin.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
     private lateinit var binding: FragmentSettingsBinding
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +21,20 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        sharedPreferences =
+            requireContext().getSharedPreferences("pivg_prefs", Context.MODE_PRIVATE)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.logoutButton.setOnClickListener {
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.remove("access_token")
+            editor.apply()
+
+            findNavController().navigate(R.id.loginFragment)
+        }
     }
 }
